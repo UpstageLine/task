883057eb67fc99bcbe547ebe4ca10e93d376f8df
Даниил <nikolaev050305@gmail.com>
2024-11-29 22:19:31 +0300

    task

---
+++ main.py
@@ -0,0 +1,19 @@
+"1st program"
+a = 9 ** 0.5 * 5
+print(a)
+
+
+"2nd program"
+b = 9.99 > 9.98 and 1000 != 1000.1
+print(b)
+
+
+"3rd program"
+c = 2 * 2 + 2
+c2 = 2 * (2 + 2)
+c3 = c == c2
+print(c3)
+
+
+"4th program"
+print(int((float('123.456'))*10)%10)
